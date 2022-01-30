d52188888769a3b68f36134abd4b7d0b189375f8
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-30 20:24:02 +0530

    [add] Shubham added UC2 of ProductReviewManagementLINQ program

--- ProductReviewManagementLINQ/Management.cs
+++ ProductReviewManagementLINQ/Management.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 
 namespace ProductReviewManagementLINQ
 {
@@ -19,6 +20,21 @@ namespace ProductReviewManagementLINQ
             {
                 Console.WriteLine("ProductId:-" + list.ProductId + "  " + "UserId:-" + list.UserId + "  " + "Rating:-" + list.Rating
                     + "  " + "Review:-" + list.Review + "  " + "IsLike:-" + list.isLike); //Print data
+            }
+        }
+
+        /* UC2:- Product Review Management.
+                 - Retrieve top 3 records from the list who’s rating is high using LINQ.
+        */
+        public static void RetrieveTop3records(List<ProductReview> productReviewlist)
+        {
+            //Query syntax for LINQ
+            var result = (from products in productReviewlist
+                          orderby products.Rating descending
+                          select products).Take(3);
+            foreach (var elements in result)
+            {
+                Console.WriteLine($"ProductId:- {elements.ProductId} UserId:- {elements.UserId} Rating:- {elements.Rating} Review:- {elements.Review} isLike:- {elements.isLike}");
             }
         }
     }
